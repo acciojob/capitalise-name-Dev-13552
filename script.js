ba520d8c40276ac735a076c98ecc8f669a20be5b
Dev-13552 <devrathour110@gmail.com>
2025-08-03 20:26:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const input = document.querySelector('#fname')
+input.addEventListener("blur", (e)=>{
+	e.preventDefault();
+	let value = (input.value).toUpperCase
+	input.value = value
+})
